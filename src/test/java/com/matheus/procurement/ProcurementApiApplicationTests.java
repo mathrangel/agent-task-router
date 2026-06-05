b89867d1303ecd82e22accafa4d76344d64ebaf5
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2026-06-05 21:25:58 +0100

    feat: initial Spring Boot setup with health endpoint

---
+++ src/test/java/com/matheus/procurement/ProcurementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.procurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
